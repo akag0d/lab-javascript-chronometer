d1f71cbc56be60507a09ad7d6ab374d21c7c7986
akag0d <ruben.neves90@gmail.com>
2022-01-25 16:32:54 +0000

    chrono

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,50 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
+
+
   }
 
   start(callback) {
-    // ... your code goes here
+    /* this.currentTime += 1; */
+    this.intervalId = setInterval (() => {
+      this.currentTime += 1;
+      callback;
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = 0;
+    if (this.currentTime >= 60);
+    minutes = Math.floor(this.currentTime / 60);
+    return minutes
+
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = 0
+
+    seconds = this.currentTime % 60
+
+    return seconds
+
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return (value < 10) ? "0" + String(value) : String(value);
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return (`${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`);
   }
 }
 
@@ -37,3 +53,5 @@ class Chronometer {
 if (typeof module !== 'undefined') {
   module.exports = Chronometer;
 }
+
+
